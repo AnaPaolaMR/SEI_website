eb053981fccc600cc3d21b4def6c7587bd47ad06
Ana Paola Mirazo Rondero <anacrayolaonechan@gmail.com>
2018-04-12 17:51:34 -0700

    - Conexion a base de datos de prueba - Separacion en pestañas de las areas de edicion de los diferentes cursos

--- recibir.php
+++ recibir.php
@@ -1,6 +1,8 @@
 <?php 
 
-	$con= mysqli_connect('localhost','root','root','sei_bd') or die('Error en la conexion al servidor');
-	$sql="INSERT INTO cursos values (101,'".$_POST["regular"]
+	$con= mysqli_connect('localhost','root','','sei_bd') or die('Error en la conexion al servidor');
+	$sql="INSERT INTO cursos (id_curso, info_curso) values (103,'".$_POST["regular_info"]."')";
+	$resultado=mysqli_query($con, $sql) or die ('Error en el query database');
+	mysqli_close($con);
 
  ?>
