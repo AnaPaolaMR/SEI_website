c212bb39918688812c4b4bf0dbb09ea7e1e2b920
Francisco Martinez <francisco.iram97@gmail.com>
2018-04-21 16:30:03 -0700

    - .php para el recibir los datos del club de conversacion y mostrarlos en las interfaces de configuracion y club de conversacion.

---
+++ recibir_club.php
@@ -0,0 +1,14 @@
+<?php
+
+	//mysqli_connect() ocupa SERVIDOR, USUARIO, CONTRASEÑA y BASE DE DATOS
+	$con= mysqli_connect('localhost','root','','sei_bd') or die('Error en la conexion al servidor');
+	$sql_1="UPDATE cursos SET id_curso=104, tipo_curso='club', info_curso='".$_POST["club_info"]."', titulo_curso='".$_POST["club_titulo"]."', video_curso='".$_POST["club_enlace"]."' WHERE id_curso=104";
+
+	//Variable de Query de SQL, requiere parametros de mysqli_connect($con) y instruccion de SQL($sql)
+	$resultado_1=mysqli_query($con, $sql_1) or die ('Error en el query database 1');
+
+	//cierra la conexion
+	mysqli_close($con);
+	header("location:admin.php"); 
+
+ ?>
