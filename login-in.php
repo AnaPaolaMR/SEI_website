9a7db79250c41ed1dfe79d6bab9f24cdefa22b8f
Ana Paola Mirazo Rondero <anacrayolaonechan@gmail.com>
2018-04-21 23:53:50 -0700

    - Validacion de usuario y contraseña con conexion a base de datos básico

--- login-in.php
+++ login-in.php
@@ -1,18 +1,45 @@
 <?php
+	
+	$host = "localhost";
+	$user = "root";
+	$pw = "";
+	$db = "sei_bd";
 
-    //Recibe datos de login.php -> email, password
-    $email = $_POST['email'];
-    echo($email);
+	$con = mysqli_connect($host, $user, $pw, $db) or die ('Error en la conexion al servidor');
 
-    $contraseña = $_POST['password'];
-    echo($contraseña);
 
-    if ($email == 'smart_eteacher@hotmail.com' && $contraseña == 'sei2018') {
-    	
-    	header("Location: admin.php");
-    }else{
-    	echo "<br> Error: Datos incorrectos";    	
-    	//header("Location: login.php");
-    }
+
+	//Toma los registros del campo email de la tabla usuario
+	$query = "SELECT email, contrasena FROM usuario WHERE email = '".$_POST['email']."' and contrasena = '".$_POST['password']."'";
+
+	$resultado = mysqli_query($con, $query);
+	$fila = mysqli_fetch_array($resultado);
+
+	echo "$fila[email]";
+
+	if ($fila == "") {
+		header("Location: login.php");
+		echo "<br> ¡Correo o contraseña incorrectos!
+
+		";  	
+		//header("Location: login.php");
+	}else{
+		header("Location: admin.php");
+	}
+/*
+	Recibe datos de login.php -> email, password
+	$correo = $_POST['email'];
+	
+	$contraseña = $_POST['password'];
+
+	if ($correo == 'smart_eteacher@hotmail.com' && $contraseña == 'sei2018') {
+		
+		header("Location: admin.php");
+	}else{
+		echo "<br> Error: Datos incorrectos";    	
+		//header("Location: login.php");
+	}
+*/
+	mysqli_close($con);
 
 ?>
