36e2e4163ff07cde4bde8742805a2ef3eaa5f6e0
Francisco Martinez <francisco.iram97@gmail.com>
2018-05-08 20:37:01 -0700

    - Se remplazo el campo email de usuario por nickname. - Validaciones en creacion y configuracion de usuarios. - Falta la eliminacion de usuarios. - Persiste Error en estilos de los modals en admin.php.

--- consulta.php
+++ consulta.php
@@ -79,6 +79,26 @@
 			
 			mysqli_close($con);
 		}
+
+		function recuperarUsuariosStdr(){
+			$host = "localhost";
+			$user = "root";
+			$pw = "";
+			$db = "sei_bd";
+
+			$con = mysqli_connect($host, $user, $pw, $db) or die ('Error en la conexion al servidor');
+
+			$query = "SELECT nombre from usuario WHERE tipo_usuario='estandar'";
+
+			$resultado = mysqli_query($con, $query);
+
+			
+			$fila = mysqli_fetch_array($resultado);
+				
+			echo "$fila[$dato]";
+			
+			mysqli_close($con);
+		}
 	}
 
 ?>
