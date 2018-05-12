626591927e621b13b17db2c3be0adb916de44263
JORGE\Jorge Beaven <beavenjorge@gmail.com>
2018-05-11 23:48:47 -0700

    Página en la que se incluye el modal de presentación, con su imagen añadida

---
+++ load.php
@@ -0,0 +1,46 @@
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="icon" href="smart.ico">
+	<link rel="stylesheet" type="text/css" href="css\bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="estilos1.css">
+	<link rel="stylesheet" type="text/css" href="estilos.css">
+
+	<title>Intro | SEI</title>
+	<meta http-equiv="refresh" content="6; URL=http://localhost:8080/index.php">
+	<meta name="keywords" content="automatic redirection">
+			<div style="backgrond-color: #a2ef44" class="container-fluid">
+			<div class="row justify-content-between">
+				<div class="col-sm-12 col-md-3 align-self-center text-center">
+					<a href="index.php"><img class="img-fluid" src="logo.png" alt="Logo SEI" width="100%"></a>
+				</div>
+</head>
+<body>
+
+	<div class="modal fade" id="mostrarmodal">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header modal-header-custom">
+					<h1>Bienvenido</h1>
+				</div>
+				<div class="modal-body">
+					<p>
+						<img class="img-fluid" src="giphy.gif" alt="Logo SEI" width="100%">
+					</p>	
+							    				
+			</div>
+		</div>
+	</div>
+	
+    <script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
+    <script>
+        $(document).ready(function () {
+            $("#mostrarmodal").modal("show");
+        });
+	</script>
+	<script src="js\jquery.js"></script>
+	<script src="js\popper.min.js"></script>
+	<script src="js\bootstrap.min.js"></script> 
+</body>
+</html>
